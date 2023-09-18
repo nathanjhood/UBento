03679e2beb351b3338de0579aa1dfd9b9cb1bcd7
Nathan <nathanjhood@googlemail.com>
2023-09-18 03:47:28 +0200

    Update menudata.js

--- menudata.js
+++ menudata.js
@@ -22,6 +22,15 @@
 
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
-var menudata={children:[
-{text:"Home",url:"https://stoneydsp.github.io/index.html"},
-{text:"UBento",url:"index.html"}]}
+var menudata={
+  children: [
+    { 
+      text: "Home",
+      url:"https://stoneydsp.com/index.html"
+    },
+    { 
+      text: "UBento",
+      url: "index.html"
+    }
+  ]
+}
